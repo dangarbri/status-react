8ffd08345b4b0b719e4f89029f0e3d4008d94993
John M. Ngei <johnmutuku628@gmail.com>
2022-03-04 12:10:28 +0300

    refactored the code to to speed up performance

--- src/status_im/ui/screens/communities/reorder.cljs
+++ src/status_im/ui/screens/communities/reorder.cljs
@@ -5,8 +5,8 @@
                      [status-im.ui.components.react :as react]
                      [status-im.ui.components.tabs :as tabs]
                      [status-im.i18n.i18n :as i18n]
-                     [status-im.ui.screens.communities.reorder-chats :as reorder-chats]
-                     [status-im.ui.screens.communities.reorder-categories :as reorder-categories]            [status-im.ui.screens.communities.community :as community]
+                     [status-im.ui.screens.communities.reorder-categories :as reorder-categories]
+                     [status-im.ui.screens.communities.community :as community]
                      [quo.design-system.colors :as colors]
                      [reagent.core :as reagent]))
 
@@ -20,9 +20,9 @@
       [tabs/tab-button state :categories (i18n/label :t/edit-categories) (= tab :categories)]]
      (cond
        (= tab :chats)
-       [reorder-chats/view]
+       [reorder-categories/view :chats]
        (= tab :categories)
-       [reorder-categories/view])]))
+       [reorder-categories/view :categories])]))
 
 (defn view []
   (let [{:keys [community-id]} (<sub [:get-screen-params])
